2e50dbcb701e0def600db664f7e1b623f368783a
Cihat Acar <cihat.acar4182@gmail.com>
2023-07-25 01:04:52 +0200

    Merge the tools exercise solution is added.

---
+++ Strings/merge_the_tools.py
@@ -0,0 +1,57 @@
+"""
+We can split s into n/k substrings where each subtring,t_i , consists of a contiguous block of k characters in s.
+Then, use each t to create string u such that:
+The characters in u are a subsequence of the characters in t_i.
+Any repeat occurrence of a character is removed from the string such that each character in u, occurs exactly once.
+In other words, if the character at some index j in t_i occurs at a previous index < j in t_i,
+then do not include the character in string u_i.
+Given s and k, print n/k lines where each line i denotes string u_i.
+Example
+s = "AAABCADDE"
+k = 3
+
+There are three substrings of length 3 to consider: 'AAA', 'BCA' and 'DDE'.
+The first substring is all 'A' characters, so u_1 = A. The second substring has all distinct characters, so u_2 = BCA.
+The third substring has different characters, so u_3 = DE.
+Note that a subsequence maintains the original order of characters encountered.
+The order of characters in each subsequence shown is important.
+
+Function Description
+Complete the merge_the_tools function in the editor below.
+merge_the_tools has the following parameters:
+string s: the string to analyze
+int k: the size of substrings to analyze
+
+Prints
+Print each subsequence on a new line. There will be n/k of them. No return value is expected.
+
+Input Format
+The first line contains a single string, s.
+The second line contains an integer, k, the length of each substring.
+
+Sample Input
+STDIN       Function
+-----       --------
+AABCAAADA   s = 'AABCAAADA'
+3           k = 3
+
+Sample Output
+AB
+CA
+AD
+"""
+
+
+def merge_the_tools(string, k):
+    # your code goes here
+    from collections import OrderedDict
+    num_subsequences = len(string) // k
+    for i in range(num_subsequences):
+        substring = string[i * k: (i + 1) * k]
+        distinct_chars = "".join(OrderedDict.fromkeys(substring))
+        print(distinct_chars)
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
